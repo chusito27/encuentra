c661f766da0dfb7c2ac9deff8382041fc0cb139f
chusito27 <herochus27@gmail.com>
2025-07-24 17:55:28 -0600

    cache updated

---
+++ lachozadelpueblo/categories.js
@@ -0,0 +1,8 @@
+// categories.js
+export const categories = [
+    { id: "all", name: 'Todas las Categorías' },
+    { id: "Adicionales", name: 'Adicionales' },
+    { id: "Casados", name: 'Casados' },
+    { id: "Desayunos", name: 'Desayunos' },
+    { id: "Gallos", name: 'Gallos' }
+];
